a7f0234819498b3633af9fcaa8c357ccf75e8869
Mukkun <52967116+Mukkun-san@users.noreply.github.com>
2020-08-29 06:14:52 +0200

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var cors = require("cors");
 
 var app = express();
 
-app.use(cors({ origin: "https://quizappexcel.herokuapp.com/" }))
+app.use(cors({ origin: "https://quiz-excel-random.web.app" }))
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
